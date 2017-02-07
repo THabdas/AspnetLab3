a39ab266b0110482f0ccf407037e4cedb39fcb96
Tristan <tristram.h@live.com>
2017-02-07 15:38:59 -0800

    first commit

---
+++ ASPLab3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPLab3.Startup))]
+namespace ASPLab3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
