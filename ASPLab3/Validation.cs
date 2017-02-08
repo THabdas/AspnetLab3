29cf57c47def5b2e2d0cf1b8f81351c20af5e1fd
Tristan Habdas <A00687587@ad.bcit.ca>
2017-02-07 20:04:40 -0800

    ninth commit

---
+++ ASPLab3/Validation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ASPLab3
+{
+    [MetadataType(typeof(Customer_Validation))]
+    public partial class Customer
+    {
+
+    }
+
+    public class Customer_Validation
+    {
+        [StringLength(30, ErrorMessage = "CustomerID must be 30 characters or less")]
+        public string CustomerID { get; set; }
+
+        [StringLength(30, ErrorMessage = "CompanyName must be 30 characters or less")]
+        public string CompanyName { get; set; }
+
+        [StringLength(30, ErrorMessage = "ContactName must be 30 characters or less")]
+        public string ContactName { get; set; }
+
+        [StringLength(30, ErrorMessage = "ContactCity must be 30 characters or less")]
+        public string ContactCity { get; set; }
+
+        [StringLength(30, ErrorMessage = "City must be 30 characters or less")]
+        public string City { get; set; }
+
+    }
+}
