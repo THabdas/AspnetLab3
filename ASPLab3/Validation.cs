5953e3a5e0d682612d422668b932714c0a1c28f4
Tristan Habdas <A00687587@ad.bcit.ca>
2017-02-07 20:04:40 -0800

    commit #13

--- ASPLab3/Validation.cs
+++ ASPLab3/Validation.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
-namespace ASPLab3
+namespace ASPLab3.Models.Northwind
 {
     [MetadataType(typeof(Customer_Validation))]
     public partial class Customer
@@ -15,20 +15,20 @@ namespace ASPLab3
 
     public class Customer_Validation
     {
-      //  [StringLength(30, ErrorMessage = "CustomerID must be 30 characters or less")]
-        //public string CustomerID { get; set; }
+        [StringLength(30, ErrorMessage = "CustomerID must be 30 characters or less")]
+        public string CustomerID { get; set; }
 
-      //  [StringLength(30, ErrorMessage = "CompanyName must be 30 characters or less")]
-        //public string CompanyName { get; set; }
+        [StringLength(30, ErrorMessage = "CompanyName must be 30 characters or less")]
+        public string CompanyName { get; set; }
 
-      //  [StringLength(30, ErrorMessage = "ContactName must be 30 characters or less")]
-       // public string ContactName { get; set; }
+        [StringLength(30, ErrorMessage = "ContactName must be 30 characters or less")]
+        public string ContactName { get; set; }
 
-       // [StringLength(30, ErrorMessage = "ContactTitle must be 30 characters or less")]
-        //public string ContactTitle { get; set; }
+        [StringLength(30, ErrorMessage = "ContactTitle must be 30 characters or less")]
+        public string ContactTitle { get; set; }
 
-       // [StringLength(30, ErrorMessage = "City must be 30 characters or less")]
-       // public string City { get; set; }
+        [StringLength(30, ErrorMessage = "City must be 30 characters or less")]
+        public string City { get; set; }
 
     }
 }
